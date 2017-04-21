4332c7ee4fb4ff37a359669656cc384ee7113d72
waynebanks <thestorerun@yahoo.com>
2017-04-21 10:19:40 -0700

    completed ex 13 of 13 for day 4 exercises

--- promise-it-wont-hurt-solutions/src/11-multiple-promises.js
+++ promise-it-wont-hurt-solutions/src/11-multiple-promises.js
@@ -1,25 +1,65 @@
-function all(promise1, promise2) {
-    this.counter = 0
-		let promises = []
+// function all(promises) {
+// 	console.log(promises)
+//     let counter = 0
+// 		// let promises = []
     
-	  function count() {
-			counter++
-			if (counter === 2) resolve(promises)
-		}
+// 	  function count() {
+// 			// counter++
+// 			if (counter === 2) resolve(promises)
+// 		}
 	
-	return new Promise((resolve, reject) => {
+// 	return new Promise((resolve, reject) => {
 	
 
-		promise1
-		.then((val) => { 
-		// 	promises[0] = val
-			counter ++
-		})
-console.log(counter)
-		promise2
-		.then((val) => { 
-			promises[1] = val
-			count()
-		})
-	})
-}
+// 		promises[0]
+// 		.then((val) => { 
+// 			// promises[0] = val
+// 			counter ++
+// 		})
+
+// 	console.log(counter)
+		
+// 		promises[1]
+// 		.then((val) => { 
+// 			// promises[1] = val
+// 			counter++
+// 		})
+// 	})
+
+// 	count()
+// }
+
+function all(para1, para2){
+
+  return new Promise(function(resolve, reject){
+
+    var counter = 0;
+    var promiseArr = [];
+
+    para1.then(function(result){
+      promiseArr.push(result);
+      counter ++;
+      if(counter === 2){
+        resolve(promiseArr);
+      }
+    });
+
+    para2.then(function(result){
+      promiseArr.push(result);
+      counter ++;
+      if(counter === 2){
+        resolve(promiseArr);
+      }
+    });
+
+
+  });
+
+
+  }
+
+
+
+
+
+all(getPromise1(), getPromise2()).then(console.log);
